b7981ac729408b760049e8349bdc46bade7d004f
Johan Mora <johanmora.jm@gmail.com>
2025-05-16 14:41:45 -0500

    ES HORA DE GANAR

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const admin = require('firebase-admin');
+const { initializeApp } = require('firebase/app');
 const authRoutes = require('./routes/auth');
 const fincaRoutes = require('./routes/fincas');
 const movimientoRoutes = require('./routes/movimientos');
@@ -12,11 +13,27 @@ const ventaRoutes = require('./routes/ventaRoutes');
 const { authenticate } = require('./middleware/auth');
 
 // Configuración Firebase Admin
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL
+};
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: 'https://inversiones-bonitoviento-sas.firebaseio.com'
 });
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
 
 const app = express();
 
