cc1d30afed39f9bb8f440c49831be3150366c921
Johan Mora <johanmora.jm@gmail.com>
2025-06-11 11:23:14 -0500

    Usuario

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -4,15 +4,24 @@ let serviceAccount;
 if (process.env.FIREBASE_SERVICE_ACCOUNT_KEY) {
   // En producción (Render), la clave viene como string JSON en la variable de entorno
   serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+  // Arreglar el formato de la private_key si viene con \n
+  if (serviceAccount.private_key && serviceAccount.private_key.includes('\\n')) {
+    serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+  }
+  if (serviceAccount.private_key && serviceAccount.private_key.includes('\n')) {
+    serviceAccount.private_key = serviceAccount.private_key.replace(/\n/g, '\n');
+  }
 } else {
   // En desarrollo local, usamos el archivo
   serviceAccount = require('../serviceAccountKey.json');
 }
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://inversiones-bonitoviento-sas-default-rtdb.firebaseio.com/'
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://inversiones-bonitoviento-sas-default-rtdb.firebaseio.com/'
+  });
+}
 
 const db = admin.database();
 const auth = admin.auth();
